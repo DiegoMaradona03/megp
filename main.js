1de2379ab66827edc679a82f79c3baa710160159
Bolsolula <160658525+DiegoMaradona03@users.noreply.github.com>
2025-10-27 12:44:41 -0300

    Add initial styles for layout and tiles

---
+++ main.js
@@ -0,0 +1,54 @@
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+  display: flex;
+}
+
+#menu-lateral {
+  width: 200px;
+  background-color: #333;
+  color: white;
+  padding: 10px;
+  box-sizing: border-box;
+  position: fixed;
+  height: 100vh;
+  overflow-y: auto;
+}
+
+#menu-lateral button {
+  width: 100%;
+  margin: 5px 0;
+  padding: 5px;
+  cursor: pointer;
+}
+
+#map-container {
+  margin-left: 200px;
+  display: grid;
+  grid-template-columns: repeat(50, 20px);
+  grid-template-rows: repeat(50, 20px);
+  border: 2px solid black;
+  overflow: auto;
+  width: calc(100vw - 200px);
+  height: 100vh;
+  background-color: #222;
+  position: relative;
+  transform-origin: 0 0;
+}
+
+.tile {
+  width: 20px;
+  height: 20px;
+}
+
+.tile.grass { background-color: #7cfc00; }
+.tile.earth { background-color: #a0522d; }
+.tile.water { background-color: #1e90ff; }
+
+.creature {
+  width: 16px;
+  height: 16px;
+  position: absolute;
+  border-radius: 50%;
+  border: 1px solid black;
+}
